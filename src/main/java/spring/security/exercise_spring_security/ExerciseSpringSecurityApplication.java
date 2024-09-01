34208330197e4a90693216c1233510e30a9e3c8f
han <131014787+ruukr8080@users.noreply.github.com>
2024-09-01 19:34:26 +0900

    first Commit

---
+++ src/main/java/spring/security/exercise_spring_security/ExerciseSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package spring.security.exercise_spring_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciseSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExerciseSpringSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/spring/security/exercise_spring_security/ExerciseSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.security.exercise_spring_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciseSpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
